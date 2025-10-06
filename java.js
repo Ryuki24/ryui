ed5185b70ebe67628c6506edfb1b550bffda0bb4
Ryuki24 <rustofaiq06@gmail.com>
2025-10-06 17:34:07 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
-let nama = prompt("Massukin nama lu");
-let umur = prompt("tulis umur lu");
+let nama = prompt("Massukin nama ");
+let umur = prompt("tulis umur ");
 
 let konfirmasi = confirm("apakah nama sama umur lu udah bener??");
-alert("Nama lu " + nama + " dan umur lu " + umur);
+alert("Nama " + nama + " dan umur " + umur);
+
 alert("ohh yaudah kalo bener");
